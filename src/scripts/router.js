aad073c19c92f2c2e73f882a0420ee544c3f89a7
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-09-10 01:28:30 +0700

    Router

---
+++ src/scripts/router.js
@@ -0,0 +1,20 @@
+const router = [
+  {
+    href: '#home',
+    name: 'Home',
+    active: true
+  },
+  {
+    href: '#favourite',
+    name: 'Favourite',
+    active: false
+  },
+  {
+    href: 'https://github.com/ikhsan-fauji',
+    name: 'About Us',
+    active: false,
+    target: '_blank'
+  }
+];
+
+export default router;
