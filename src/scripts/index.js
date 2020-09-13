c7771c5e9e2b3068b9ab0d4a88fabd9eee12ae16
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-09-13 20:21:15 +0700

    on scroll add sticky and drawer handler

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,23 @@ import '../styles/main.scss';
 import navigation from './navigation';
 
 const main = () => {
-  navigation.init();
+  // navigation.init();
+  window.onscroll = () => {
+    navigation.sticky();
+  };
+
+  const drawerMenu = document.getElementById('drawer-menu');
+  const drawer = document.getElementById('drawer');
+  drawerMenu.addEventListener('click', (event) => {
+    drawer.classList.toggle('show-drawer');
+    event.stopPropagation();
+  });
+
+  const mainElement = document.querySelector('body');
+  mainElement.addEventListener('click', (event) => {
+    drawer.classList.remove('show-drawer');
+    event.stopPropagation();
+  });
 };
 
 document.addEventListener('DOMContentLoaded', main);
