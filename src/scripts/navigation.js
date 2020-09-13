c6bc4ec0c76e5fc06275858f308fb5c35442f01a
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-09-13 20:18:27 +0700

    Sticky navbar

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -8,6 +8,17 @@ class Navigation {
     mainNavigation.navigations = router;
     menuBar.appendChild(mainNavigation);
   }
+
+  static sticky() {
+    const header = document.getElementById('menu-bar');
+    const sticky = header.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+      header.classList.add('sticky');
+    } else {
+      header.classList.remove('sticky');
+    }
+  }
 }
 
 export default Navigation;
