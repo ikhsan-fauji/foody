3a0686aa747c19c4186e5a447d930e44021f5b02
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-09-14 21:02:29 +0700

    menu js

---
+++ src/scripts/menu.js
@@ -0,0 +1,20 @@
+import './component/MenuCard';
+
+class Menu {
+  constructor(menus) {
+    this._popularMenus = menus || [];
+  }
+
+  popular() {
+    if (this._popularMenus.length > 0) {
+      const popularMenus = document.querySelector('.menus');
+      this._popularMenus.forEach((menu) => {
+        const menuCard = document.createElement('menu-card');
+        menuCard.menu = menu;
+        popularMenus.appendChild(menuCard);
+      });
+    }
+  }
+}
+
+export default Menu;
