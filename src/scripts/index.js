a70840d05b180c8c378b60fb7b9383f8d9ddb7e2
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-09-14 21:09:22 +0700

    update index

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,23 @@
 import 'regenerator-runtime';
+import './lazysizes.min';
 import '../styles/main.scss';
 
 import navigation from './navigation';
+import Restaurant from './restaurant';
+import Menu from './menu';
+import dummy from './data/DATA.json';
+
+const descendingByRating = (a, b) => {
+  let comparison = 0;
+
+  if (a.rating > b.rating) {
+    comparison = -1;
+  } else if (a.rating < b.rating) {
+    comparison = 1;
+  }
+
+  return comparison;
+};
 
 const main = () => {
   // navigation.init();
@@ -21,6 +37,13 @@ const main = () => {
     drawer.classList.remove('show-drawer');
     event.stopPropagation();
   });
+
+  const sortedRestaurants = dummy.restaurants.sort(descendingByRating);
+  const sortedPopularMenus = dummy.popularMenus.sort(descendingByRating);
+  const restaurant = new Restaurant(sortedRestaurants);
+  restaurant.renderAll();
+  const menu = new Menu(sortedPopularMenus);
+  menu.popular();
 };
 
 document.addEventListener('DOMContentLoaded', main);
