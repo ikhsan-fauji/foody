bd660ad4b400f737b56433050fc8599221dda35c
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-10-03 21:49:26 +0700

    request helper

---
+++ src/scripts/helper/request-helper.js
@@ -0,0 +1,45 @@
+import { API_KEY } from '../utils/enums';
+
+const request = {
+  async get(url) {
+    let responseData = null;
+
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        responseData = response.json();
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+
+    return responseData;
+  },
+
+  async post(url, data) {
+    let responseData = null;
+
+    try {
+      const postConfig = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Auth-Token': API_KEY
+        },
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+      };
+
+      const response = await fetch(url, postConfig);
+      if (response.ok) {
+        responseData = response.json();
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+
+    return responseData;
+  }
+};
+
+export default request;
