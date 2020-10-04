6252b7b75e6e72d99bae8b59ff60091f51884bc4
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-10-04 23:20:25 +0700

    minimize restaurant detail page

--- src/scripts/views/pages/restaurant-detail.js
+++ src/scripts/views/pages/restaurant-detail.js
@@ -1,18 +1,11 @@
 import UrlParser from '../../routes/url-parser';
 import RestaurantData from '../../data/restaurant-data';
 import HeaderContent from '../templates/header-content';
-import '../../component/ReviewForm';
 
 const RestaurantDetail = {
-  _detailTemplate() {
-    const template = `<section id="restaurant-content"></section>`;
-
-    return template;
-  },
-
   async render() {
     HeaderContent.breadCrumb('Restaurant Detail');
-    return this._detailTemplate();
+    return `<section id="restaurant-content"></section>`;
   },
 
   afterRendered() {
@@ -20,8 +13,6 @@ const RestaurantDetail = {
     const restaurantId = url.verb;
     const restaurant = new RestaurantData();
     restaurant.detail(restaurantId);
-    // const reviewForm = document.createElement('review-form');
-    // document.querySelector('#review-form').appendChild(reviewForm);
   }
 };
 
