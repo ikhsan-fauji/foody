9c9f96003daeb24fc0cc2611f73fcc7ff5d0fd37
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-10-04 15:48:32 +0700

    Consumer reviews compponent

---
+++ src/scripts/component/ConsumerReviews.js
@@ -0,0 +1,34 @@
+class ConsumerReviews extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  set reviews(reviews = []) {
+    this._reviews = reviews;
+    this.render();
+  }
+
+  _reviewsTemplate() {
+    let template = `<ul class="reviews">`;
+
+    this._reviews.forEach((reviewDetail) => {
+      const { name = null, date = null, review = null } = reviewDetail;
+      template += `
+        <li class="review-item">
+          <h4 class="reviewer">${name}</h4>
+          <i class="review-date">${date}</i>
+          <p class="review-detail">${review}</p>
+        </li>
+      `;
+    });
+
+    template += `</ul>`;
+    return template;
+  }
+
+  render() {
+    this.innerHTML = this._reviewsTemplate();
+  }
+}
+
+customElements.define('consumer-reviews', ConsumerReviews);
