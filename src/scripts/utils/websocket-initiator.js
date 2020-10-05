9315e9bfee17ca992428158df1aa737c3f837c7a
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-10-05 20:10:28 +0700

    web socket initiator

---
+++ src/scripts/utils/websocket-initiator.js
@@ -0,0 +1,26 @@
+import NotificationHelper from '../helper/notification-helper';
+
+const WebSocketInitiator = {
+  init(url) {
+    const webSocket = new WebSocket(url);
+    webSocket.onopen = this._onOpen;
+    webSocket.onmessage = this._onMessageHandler;
+  },
+
+  _onOpen(event) {
+    console.log('CONNECTED');
+  },
+
+  _onMessageHandler(message) {
+    console.debug(message);
+    // const { title, overview, poster_path } = JSON.parse(message.data);
+    // NotificationHelper.sendNotification({
+    //   title: `${title} is on cinema!`,
+    //   options: {
+    //     body: overview
+    //     // image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`
+    //   }
+    // });
+  }
+};
+export default WebSocketInitiator;
