0b3a097899dfbf23c5e8329f52a4a6c7a0f0e5ab
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-10-05 20:05:42 +0700

    global config

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  API_URL: 'https://dicoding-restaurant-api.el.r.appspot.com',
+  API_KEY: 12345,
+  DB_NAME: 'foody_db',
+  DB_VERSION: 1,
+  DB_STORE_NAME: 'favorite_restaurant',
+  DEFAULT_LANGUAGES: 'en-us',
+  WEB_SOCKET_SERVER: 'wss://stormy-badlands-06326.herokuapp.com'
+};
+
+export default CONFIG;
