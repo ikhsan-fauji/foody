c13998b7ba514ff0f5e93630df3cd99019167685
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-10-06 23:03:01 +0700

    no data template

---
+++ src/scripts/views/templates/nodata-template.js
@@ -0,0 +1,10 @@
+const noDataTemplate = () => {
+  return `
+    <div class="not-found">
+      <h1>Oops!!!</h1>
+      <p>Data tidak tersedia.</p>
+    </div>
+  `;
+};
+
+export default noDataTemplate;
