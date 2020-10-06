b6d2eaeac093406096a1f3dfdd6511ccc18487ce
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-10-06 23:04:23 +0700

    implement loader and no data

--- src/scripts/data/restaurant-data.js
+++ src/scripts/data/restaurant-data.js
@@ -2,9 +2,11 @@ import '../component/RestaurantCard';
 import '../component/RestaurantDetail';
 import { restaurantApi } from '../utils/enums';
 import idb from '../helper/idb-helper';
+import loader from '../helper/loader-helper';
 import request from '../helper/request-helper';
 import descendingByRating from '../helper/sorter-helper';
 import alert from '../helper/alert-helper';
+import noDataTemplate from '../views/templates/nodata-template';
 
 class RestaurantData {
   async _fetchListRestaurant() {
@@ -17,31 +19,55 @@ class RestaurantData {
     return response;
   }
 
-  async recommended() {
-    const restaurants = await this._fetchListRestaurant();
-    this._restaurants = restaurants.slice(0, 4);
-    this._renderList();
+  _noDataTemplate(id) {
+    const content = document.querySelector(id);
+    content.innerHTML = noDataTemplate();
   }
 
-  _renderList() {
-    if (this._restaurants.length > 0) {
-      const listRestaurant = document.querySelector('.restaurants');
-      this._restaurants.forEach((restaurant) => {
-        const restaurantCard = document.createElement('restaurant-card');
-        restaurantCard.restaurant = restaurant;
-        listRestaurant.appendChild(restaurantCard);
-      });
+  async recommended() {
+    const elementId = '#explore';
+    loader.start(elementId);
+    const restaurants = await this._fetchListRestaurant();
+    loader.stop();
+    this._restaurants = restaurants.slice(0, 4);
+    if (this._restaurants && this._restaurants.length > 0) {
+      this._renderList();
+    } else {
+      this._noDataTemplate(elementId);
     }
   }
 
+  _renderList() {
+    const listRestaurant = document.querySelector('.restaurants');
+    this._restaurants.forEach((restaurant) => {
+      const restaurantCard = document.createElement('restaurant-card');
+      restaurantCard.restaurant = restaurant;
+      listRestaurant.appendChild(restaurantCard);
+    });
+  }
+
   async list() {
+    const elementId = '#explore';
+    loader.start(elementId);
     this._restaurants = await this._fetchListRestaurant();
-    this._renderList();
+    loader.stop();
+    if (this._restaurants && this._restaurants.length > 0) {
+      this._renderList();
+    } else {
+      this._noDataTemplate(elementId);
+    }
   }
 
   async favorites() {
+    const elementId = '#explore';
+    loader.start('#explore');
     this._restaurants = await idb.getAll();
-    this._renderList();
+    loader.stop();
+    if (this._restaurants && this._restaurants.length > 0) {
+      this._renderList();
+    } else {
+      this._noDataTemplate(elementId);
+    }
   }
 
   findFavoriteByKey(key) {
@@ -49,16 +75,22 @@ class RestaurantData {
   }
 
   async detail(id) {
+    const elementId = '#restaurant-content';
+    loader.start(elementId);
     const response = await request.get(`${restaurantApi.detail}${id}`);
+    loader.stop();
     if (response && response.error) {
+      this._noDataTemplate(elementId);
       console.error(response.message);
-    } else {
+    } else if (response.restaurant) {
       this._restaurant = response.restaurant;
       const detailElement = document.createElement('restaurant-detail');
       detailElement.restaurantId = id;
       detailElement.detail = this._restaurant;
       const restaurantContent = document.querySelector('#restaurant-content');
       restaurantContent.appendChild(detailElement);
+    } else {
+      this._noDataTemplate(elementId);
     }
   }
 
