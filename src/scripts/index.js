96b135c866c5b75c6e46dddef198bf8ef21ac597
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-10-25 22:52:25 +0700

    remove initialize indexedDB

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,11 +6,7 @@ import '../styles/main.scss';
 import CONFIG from './globals/config';
 import swRegister from './utils/service-worker-register';
 import WebSocketInitiator from './utils/websocket-initiator';
-import idb from './helper/idb-helper';
 import App from './views/app';
-
-// initializing indexed db
-idb.initialize();
 
 // application init
 const menuBar = document.querySelector('#menu-bar');
