a4ea0afb14aff97dde77d822cdee11485b78de11
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-10-28 23:27:09 +0700

    error helper

---
+++ src/scripts/helper/error-helper.js
@@ -0,0 +1,13 @@
+import noDataTemplate from '../views/templates/nodata-template';
+
+const handleError = ({ elementId, functionName, error }) => {
+  const element = document.querySelector(elementId);
+  if (error.message === 'Failed to fetch') {
+    element.innerHTML = noDataTemplate('Connection Failed');
+  } else {
+    element.innerHTML = noDataTemplate('Something went wrong');
+  }
+  console.error(functionName, error);
+};
+
+export default handleError;
