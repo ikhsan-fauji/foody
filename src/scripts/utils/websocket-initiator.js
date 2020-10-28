7cad97829742ca0335a58e76726d0032848f369d
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-10-28 22:44:37 +0700

    remark notification web socket for a moment

--- src/scripts/utils/websocket-initiator.js
+++ src/scripts/utils/websocket-initiator.js
@@ -11,16 +11,16 @@ const WebSocketInitiator = {
 
   _onOpen() {
     console.log('CONNECTED');
-    NotificationHelper.sendNotification({
-      title: `websocket.org`,
-      options: {
-        body:
-          'WebSocket was connected. 3 sample message would be send in 5 seconds'
-      }
-    });
-    setTimeout(() => {
-      this._sendMessage();
-    }, 5000);
+    // NotificationHelper.sendNotification({
+    //   title: `websocket.org`,
+    //   options: {
+    //     body:
+    //       'WebSocket was connected. 3 sample message would be send in 5 seconds'
+    //   }
+    // });
+    // setTimeout(() => {
+    //   this._sendMessage();
+    // }, 5000);
   },
 
   _sendMessage(counter = 1) {
