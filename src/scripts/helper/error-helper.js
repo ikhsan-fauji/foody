fd17c4f34faa0e16805de2ddae3d9c48b8183347
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-11-26 23:15:34 +0700

    return if no element

--- src/scripts/helper/error-helper.js
+++ src/scripts/helper/error-helper.js
@@ -2,6 +2,7 @@ import noDataTemplate from '../views/templates/nodata-template';
 
 const handleError = ({ elementId, functionName, error }) => {
   const element = document.querySelector(elementId);
+  if (!element) return;
   if (error.message === 'Failed to fetch') {
     element.innerHTML = noDataTemplate('Connection Failed');
   } else {
