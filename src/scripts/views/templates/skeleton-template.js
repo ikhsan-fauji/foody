d8caffd77bb224a4404a7c7382317444214ff998
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-12-13 02:28:52 +0700

    update skeleton template

--- src/scripts/views/templates/skeleton-template.js
+++ src/scripts/views/templates/skeleton-template.js
@@ -3,14 +3,14 @@ const restaurantCard = (count = 1) => {
   for (let index = 1; index <= count; index++) {
     template += `
       <article class="card restaurant">
-        <div class="card-image restaurant-thumbnail skeleton-10"></div>
+        <div class="card-image restaurant-thumbnail skeleton-10 skeleton-loading"></div>
         <div class="card-body restaurant-content">
-        <div class="restaurant-title skeleton-7"></div>
-        <div class="restaurant-title skeleton-3"></div>
+        <div class="restaurant-title skeleton-7 skeleton-loading"></div>
+        <div class="restaurant-title skeleton-3 skeleton-loading"></div>
 
-        <div class="restaurant-description card-text skeleton-8"></div>
-        <div class="restaurant-description card-text skeleton-10"></div>
-        <div class="restaurant-description card-text skeleton-8"></div>
+        <div class="restaurant-description card-text skeleton-8 skeleton-loading"></div>
+        <div class="restaurant-description card-text skeleton-10 skeleton-loading"></div>
+        <div class="restaurant-description card-text skeleton-8 skeleton-loading"></div>
       </div>
       </article>
     `;
@@ -22,31 +22,31 @@ const restaurantDetail = () => {
   return `
   <div class="container">
     <section class="restaurant-detail">
-      <div class="restaurant-image skeleton-10"></div>
-      <div class="restaurant-name skeleton-5"></div>
-      <div class="restaurant-info skeleton-7"></div>
-      <div class="restaurant-description card-text skeleton-8"></div>
-      <div class="restaurant-description card-text skeleton-10"></div>
-      <div class="restaurant-description card-text skeleton-8"></div>
+      <div class="restaurant-image skeleton-10 skeleton-loading"></div>
+      <div class="restaurant-name skeleton-5 skeleton-loading"></div>
+      <div class="restaurant-info skeleton-7 skeleton-loading"></div>
+      <div class="restaurant-description card-text skeleton-8 skeleton-loading"></div>
+      <div class="restaurant-description card-text skeleton-10 skeleton-loading"></div>
+      <div class="restaurant-description card-text skeleton-8 skeleton-loading"></div>
 
       <section class="restaurant-menus">
         <div class="foods">
           <div id="food-list">
-            <div class="skeleton-10"></div>
-            <div class="skeleton-10"></div>
-            <div class="skeleton-10"></div>
-            <div class="skeleton-10"></div>
-            <div class="skeleton-10"></div>
+            <div class="skeleton-10 skeleton-loading"></div>
+            <div class="skeleton-10 skeleton-loading"></div>
+            <div class="skeleton-10 skeleton-loading"></div>
+            <div class="skeleton-10 skeleton-loading"></div>
+            <div class="skeleton-10 skeleton-loading"></div>
           </div>
         </div>
 
         <div class="drinks">
           <div id="drink-list">
-            <div class="skeleton-10"></div>
-            <div class="skeleton-10"></div>
-            <div class="skeleton-10"></div>
-            <div class="skeleton-10"></div>
-            <div class="skeleton-10"></div>
+            <div class="skeleton-10 skeleton-loading"></div>
+            <div class="skeleton-10 skeleton-loading"></div>
+            <div class="skeleton-10 skeleton-loading"></div>
+            <div class="skeleton-10 skeleton-loading"></div>
+            <div class="skeleton-10 skeleton-loading"></div>
           </div>
         </div>
       </section>
