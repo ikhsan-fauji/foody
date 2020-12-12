ce33e32b32b3096b88097f01cdf884e85975f802
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-12-12 20:53:54 +0700

    main css, menu id, button id

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 import 'regenerator-runtime';
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import '../styles/main.scss';
+import '../styles/main.css';
 import './component/skeleton';
 import './component/hero-breadcrumb';
 
@@ -11,8 +11,8 @@ import WebSocketInitiator from './utils/websocket-initiator';
 import App from './views/app';
 
 // application init
-const menuBar = document.querySelector('#menu-bar');
-const button = document.querySelector('#drawer-menu');
+const menuBar = document.querySelector('#navbar');
+const button = document.querySelector('#burger-menu');
 const drawer = document.querySelector('#drawer');
 const content = document.querySelector('#main-content');
 const app = new App({ menuBar, button, drawer, content });
